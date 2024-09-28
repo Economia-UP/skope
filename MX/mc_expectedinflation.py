75b36abe7b6344a52bd2abdc92ed0e9695731279
jadrk040507 <jadrk040507@gmail.com>
2024-09-28 09:37:15 -0600

    update

--- MX/mc_expectedinflation.py
+++ MX/mc_expectedinflation.py
@@ -4,31 +4,31 @@ import pandas as pd
 
 # BANXICO_API_KEY = '86d02771fd6b64ce29912469f70d872cf666627201a5d7e819a82c452ae61289'
 
-def monetary():
+def EXP_LOWER():
     # Retrieve the INEGI API key from environment variables
     BANXICO_API_KEY = os.getenv('BANXICO_API_KEY')
     
-    ASSET_ID = 'SF29654'  # Your BANXICO series ID
-    ASSET = f'https://www.banxico.org.mx/SieAPIRest/service/v1/series/{ASSET_ID}/datos?token={BANXICO_API_KEY}&mediaType=json'
-    RESERVES_ID = 'SF29656'  # Your BANXICO series ID
-    RESERVES = f'https://www.banxico.org.mx/SieAPIRest/service/v1/series/{RESERVES_ID}/datos?token={BANXICO_API_KEY}&mediaType=json'
+    EXP_LOWER_ID = 'SR14196'  # Your BANXICO series ID
+    EXP_LOWER = f'https://www.banxico.org.mx/SieAPIRest/service/v1/series/{EXP_LOWER_ID}/datos?token={BANXICO_API_KEY}&mediaType=json'
+    EXP_UPPER_ID = 'SR14197'  # Your BANXICO series ID
+    EXP_UPPER = f'https://www.banxico.org.mx/SieAPIRest/service/v1/series/{EXP_UPPER_ID}/datos?token={BANXICO_API_KEY}&mediaType=json'
     
     # Fetch data from the BANXICO API
-    response1 = requests.get(ASSET)
+    response1 = requests.get(EXP_LOWER)
     response1.raise_for_status()  # Raise an error if the response status is not 200
     data1 = response1.json()
 
     observations1 = data1.get('bmx').get('series')[0].get('datos')
     if not observations1:
-        raise ValueError(f"No observations found for series ID {ASSET_ID}")
+        raise ValueError(f"No observations found for series ID {EXP_LOWER_ID}")
 
-    response2 = requests.get(RESERVES)
+    response2 = requests.get(EXP_UPPER)
     response2.raise_for_status()  # Raise an error if the response status is not 200
     data2 = response2.json()
 
     observations2 = data2.get('bmx').get('series')[0].get('datos')
     if not observations2:
-        raise ValueError(f"No observations found for series ID {RESERVES_ID}")    
+        raise ValueError(f"No observations found for series ID {EXP_UPPER_ID}")    
 
 
     df1 = pd.DataFrame(observations1)
@@ -39,21 +39,21 @@ def monetary():
     df2['fecha'] = pd.to_datetime(df2['fecha'], format='%d/%m/%Y', errors='coerce')
     df2['dato'] = pd.to_numeric(df2['dato'].str.replace(',', ''), errors='coerce')
     
-    # Filter since 2018
-    df1 = df1[df1['fecha'] >= '2018-01-01']
-    df2 = df2[df2['fecha'] >= '2018-01-01']
+    # Filter since 2024
+    df1 = df1[df1['fecha'] >= '2024-01-01']
+    df2 = df2[df2['fecha'] >= '2024-01-01']
 
     dfs = df1.merge(df2, how='outer', on='fecha')
-    dfs.columns = ['fecha', 'Activos internacionales netos', 'Reservas internacionales']
+    dfs.columns = ['fecha', 'Primer cuartil', 'Tercer cuartil']
     
     # Define the output directory and ensure it exists
     output_dir = 'MX'
     os.makedirs(output_dir, exist_ok=True)  # Create the directory if it doesn't exist
-    output_file = os.path.join(output_dir, 'mc_internationalreserves.csv')
+    output_file = os.path.join(output_dir, 'mc_internationalEXP_UPPER.csv')
 
     # Save the merged DataFrame to a CSV file in the specified folder
     dfs.to_csv(output_file, index=False)
     print(f"Data successfully written to {output_file}")
 
 if __name__ == "__main__":
-    monetary()
+    EXP_LOWER()
