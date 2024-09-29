edf897958b8c2e6aed3d157eb41ad5d898c1c181
jadrk040507 <jadrk040507@gmail.com>
2024-09-29 07:46:39 -0600

    Monetary aggregates Update

--- MX/mc_monetaryaggregates.py
+++ MX/mc_monetaryaggregates.py
@@ -8,9 +8,9 @@ def AGGREGATES():
     # Retrieve the INEGI API key from environment variables
     BANXICO_API_KEY = os.getenv('BANXICO_API_KEY')
     
-    M1_ID = 'SR14196'  # Your BANXICO series ID
+    M1_ID = 'SF311408'  # Your BANXICO series ID
     M1 = f'https://www.banxico.org.mx/SieAPIRest/service/v1/series/{M1_ID}/datos?token={BANXICO_API_KEY}&mediaType=json'
-    M2_ID = 'SR14197'  # Your BANXICO series ID
+    M2_ID = 'SF311418'  # Your BANXICO series ID
     M2 = f'https://www.banxico.org.mx/SieAPIRest/service/v1/series/{M2_ID}/datos?token={BANXICO_API_KEY}&mediaType=json'
     
     # Fetch data from the BANXICO API
@@ -39,17 +39,24 @@ def AGGREGATES():
     df2['fecha'] = pd.to_datetime(df2['fecha'], format='%d/%m/%Y', errors='coerce')
     df2['dato'] = pd.to_numeric(df2['dato'].str.replace(',', ''), errors='coerce')
     
-    # Filter since 2024
-    df1 = df1[df1['fecha'] >= '2024-01-01']
-    df2 = df2[df2['fecha'] >= '2024-01-01']
+    # Filter since 2017
+    df1 = df1[df1['fecha'] >= '2017-01-01']
+    df2 = df2[df2['fecha'] >= '2017-01-01']
 
     dfs = df1.merge(df2, how='outer', on='fecha')
-    dfs.columns = ['fecha', 'Primer cuartil', 'Tercer cuartil']
-    
+    dfs.columns = ['fecha', 'M1 (datos)', 'M2 (datos)']
+
+    # Calculate pct_change
+    index = pd.date_range(start='2017-01-01', periods=len(dfs['fecha']), freq='M')
+    dfs = dfs.set_index(index)
+
+    dfs['M1'] = dfs['M1 (datos)'].pct_change(periods=12) * 100  # Multiply by 100 for percentage
+    dfs['M2'] = dfs['M2 (datos)'].pct_change(periods=12) * 100  # Multiply by 100 for percentage
+
     # Define the output directory and ensure it exists
     output_dir = 'MX'
     os.makedirs(output_dir, exist_ok=True)  # Create the directory if it doesn't exist
-    output_file = os.path.join(output_dir, 'mc_expectedinflation.csv')
+    output_file = os.path.join(output_dir, 'mc_monetaryaggregates.csv')
 
     # Save the merged DataFrame to a CSV file in the specified folder
     dfs.to_csv(output_file, index=False)
