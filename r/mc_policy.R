a2bacbfb15d2b2d626fda18806dde21b6d49d55a
jadrk040507 <jadrk040507@gmail.com>
2024-12-12 23:05:04 -0600

    Fix data

--- r/mc_policy.R
+++ r/mc_policy.R
@@ -21,26 +21,27 @@ series.df <- reduce(list(ref, inf, exp), full_join, by = "date")
 
 colnames(series.df)[2:4] <- c("Tasa objetivo", "Inflación", "Inflación esperada")
 
+# Ordena por fecha
 series.df <- series.df %>%
-  arrange(date) %>%
+  arrange(date)
+
+# Rellena hacia abajo y luego interpola los valores faltantes
+series.df <- series.df %>%
+  complete(date = seq(min(date), max(date), by = "1 day")) %>% # Asegura una secuencia completa de fechas
+  group_by() %>% # Elimina cualquier agrupamiento previo
   mutate(
-    date_diff = as.numeric(date - lag(date, default = first(date))),
-    is_consecutive = date_diff == 1
+    `Tasa objetivo` = na.approx(`Tasa objetivo`, na.rm = FALSE, rule = 2),
+    `Inflación` = na.approx(`Inflación`, na.rm = FALSE, rule = 2),
+    `Inflación esperada` = na.approx(`Inflación esperada`, na.rm = FALSE, rule = 2)
   )
 
-# Rellena valores hacia abajo solo para fechas consecutivas
+# Filtra las fechas deseadas
 series.df <- series.df %>%
-  group_by(is_consecutive) %>%
-  fill("Tasa objetivo", "Inflación", "Inflación esperada", .direction = "down") %>%
-  ungroup() %>%
   filter(date >= "2020-01-01")
+
 
 series.df$"Tasa real ex-ante" = series.df$"Tasa objetivo" - series.df$"Inflación esperada"
 series.df$"Tasa real ex-post" = series.df$"Tasa objetivo" - series.df$"Inflación"
-
-
-series.df <- series.df %>%
-  select(-date_diff, -is_consecutive)
 
 # Specify the output directory and file name
 write.csv(series.df, "data/mc_policy.csv", row.names = FALSE)
