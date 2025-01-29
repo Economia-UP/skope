8da6dd008bf7dd8cb0ca12b477fe59619eaeb00e
jadrk040507 <jadrk040507@gmail.com>
2025-01-29 13:53:32 -0600

    New plots

--- r/growth/growth.R
+++ r/growth/growth.R
@@ -1,27 +1,107 @@
 print("growth.R")
-rm(list=ls())
+rm(list = ls())
 
-library("siebanxicor")
+# Load the necessary libraries
+library(inegiR)
 library(dplyr)
-library(purrr)
 library(tidyr)
+library(ggplot2)
+library(hrbrthemes)
+library(gcookbook)
+library(forcats)
 library(lubridate)
+library(sysfonts)
+
+font_add_google("Roboto Condensed")
+import_roboto_condensed()
 
 # Define your INEGI API key
-setToken(Sys.getenv("BANXICO_API"))
+inegi.api = Sys.getenv("INEGI_API")
 
 # Fetch the data using the specified series IDs
-idSeries <- c("SR14202", "SR14203","SR14204") 
+# idSeries <- c("910399", "910400", "910403")  # Your INEGI series IDs
 
-# Get the data
-series <- getSeriesData(idSeries)
-med <- getSerieDataFrame(series, "SR14202")
-first <- getSerieDataFrame(series, "SR14203")
-third <- getSerieDataFrame(series, "SR14204")
+gdp <- inegi_series(series = "736181", token = inegi.api)
 
-series.df <- reduce(list(first, med, third), full_join, by = "date") %>% 
-  filter(date >= Sys.Date() - years(1))
-colnames(series.df)[2:4] <- c("Primer cuartil", "Mediana", "Tercer cuartil")
+orden_sexenios <- c(
+  "Miguel de la Madrid (1982-1988)",
+  "Carlos Salinas (1988-1994)",
+  "Ernesto Zedillo (1994-2000)",
+  "Vicente Fox (2000-2006)",
+  "Felipe Calderón (2006-2012)",
+  "Enrique Peña Nieto (2012-2018)",
+  "Andrés Manuel López Obrador (2018-2024)",
+  "Claudia Sheinbaum Pardo (2024-2030)"
+)
 
-# Specify the output directory and file name
-(write.csv(series.df, "data/mc_expectedinflation_sub.csv", row.names = FALSE))
+gdp <- gdp %>% 
+  group_by(date_shortcut) %>%
+  arrange(date) %>% 
+  mutate(growth  = (values/lag(values) - 1)*100) %>% 
+  mutate(year = year(date),  # Extrae el año de la fecha
+         sexenio = case_when(
+           year >= 1982 & year < 1988 ~ "Miguel de la Madrid (1982-1988)",
+           year >= 1988 & year < 1994 ~ "Carlos Salinas (1988-1994)",
+           year >= 1994 & year < 2000 ~ "Ernesto Zedillo (1994-2000)",
+           year >= 2000 & year < 2006 ~ "Vicente Fox (2000-2006)",
+           year >= 2006 & year < 2012 ~ "Felipe Calderón (2006-2012)",
+           year >= 2012 & year < 2018 ~ "Enrique Peña Nieto (2012-2018)",
+           year >= 2018 & year < 2024 ~ "Andrés Manuel López Obrador (2018-2024)",
+           year >= 2024 & year < 2030 ~ "Claudia Sheinbaum Pardo (2024-2030)",
+           TRUE ~ "Otro"
+         ),
+         sexenio = factor(sexenio, levels = orden_sexenios) # Ordenar como factor
+  )
+
+sexenios_gdp <- gdp %>%
+  group_by(sexenio) %>%
+  reframe(mean_growth = mean(growth, na.rm = TRUE)) %>% 
+  filter(!is.na(sexenio)) %>% 
+  arrange(sexenio)
+
+# Crecimiento económico
+ggplot(gdp, aes(date, growth)) +
+  geom_line(size = 1) +
+  labs( title = "Crecimiento económico en México",
+        subtitle = "Variación anual",
+        y = "",
+        x = "",
+        caption = "Fuente: INEGI") +
+  theme_ipsum_rc(grid="Y") %>% 
+  gg_check()
+ggsave("plots/gdp_growth.png")
+
+
+# Crecimiento promedio por sexenio
+ggplot(sexenios_gdp, aes(mean_growth/100, fct_rev(sexenio))) +
+  geom_col() +
+  labs( title = "Crecimiento promedio por sexenio",
+        subtitle = "Promedio del crecimiento anual en México",
+        y = "",
+        x = "",
+        caption = "Fuente: INEGI") +
+  scale_x_percent() +
+  theme_ipsum_rc(grid="X") %>% 
+  gg_check()
+ggsave("plots/sexenios.png")
+
+
+pop <- inegi_series_multiple(series = "289242", token = inegi.api) %>% 
+  select(date, date_shortcut, values)
+
+gdppc <- left_join(gdp, pop, join_by(date), suffix = c("_gdp", "_pop")) %>% 
+  reframe(date, date_shortcut = date_shortcut_gdp, gdppc = values_gdp/values_pop*1000000) %>% 
+  filter(gdppc != 0)
+
+# Crecimiento económico per cápita
+ggplot(gdppc, aes(date, gdppc)) +
+  geom_line(size = 1) +
+  labs( title = "PIB per cápita en México",
+        subtitle = "Moneda nacional",
+        y = "",
+        x = "",
+        caption = "Fuente: INEGI") +
+  scale_y_comma() +
+  theme_ipsum_rc(grid="Y") %>% 
+  gg_check()
+ggsave("plots/gdppc.png")
