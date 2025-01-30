21ec0df7591f4f06ab4f5789ab8e039fb9cdb128
jadrk040507 <jadrk040507@gmail.com>
2025-01-29 21:25:24 -0600

    Changes

--- r/growth/growth.R
+++ r/growth/growth.R
@@ -7,7 +7,6 @@ library(dplyr)
 library(tidyr)
 library(ggplot2)
 library(hrbrthemes)
-library(gcookbook)
 library(forcats)
 library(lubridate)
 library(sysfonts)
@@ -16,7 +15,8 @@ font_add_google("Roboto Condensed")
 import_roboto_condensed()
 
 # Define your INEGI API key
-inegi.api = Sys.getenv("INEGI_API")
+# inegi.api = Sys.getenv("INEGI_API")
+inegi.api <- "446548c3-7b55-4b22-8430-ac8f251ea555"
 
 # Fetch the data using the specified series IDs
 # idSeries <- c("910399", "910400", "910403")  # Your INEGI series IDs
@@ -40,14 +40,14 @@ gdp <- gdp %>%
   mutate(growth  = (values/lag(values) - 1)*100) %>% 
   mutate(year = year(date),  # Extrae el año de la fecha
          sexenio = case_when(
-           year >= 1982 & year < 1988 ~ "Miguel de la Madrid (1982-1988)",
-           year >= 1988 & year < 1994 ~ "Carlos Salinas (1988-1994)",
-           year >= 1994 & year < 2000 ~ "Ernesto Zedillo (1994-2000)",
-           year >= 2000 & year < 2006 ~ "Vicente Fox (2000-2006)",
-           year >= 2006 & year < 2012 ~ "Felipe Calderón (2006-2012)",
-           year >= 2012 & year < 2018 ~ "Enrique Peña Nieto (2012-2018)",
-           year >= 2018 & year < 2024 ~ "Andrés Manuel López Obrador (2018-2024)",
-           year >= 2024 & year < 2030 ~ "Claudia Sheinbaum Pardo (2024-2030)",
+           year > 1982 & year <= 1988 ~ "Miguel de la Madrid (1982-1988)",
+           year > 1988 & year <= 1994 ~ "Carlos Salinas (1988-1994)",
+           year > 1994 & year <= 2000 ~ "Ernesto Zedillo (1994-2000)",
+           year > 2000 & year <= 2006 ~ "Vicente Fox (2000-2006)",
+           year > 2006 & year <= 2012 ~ "Felipe Calderón (2006-2012)",
+           year > 2012 & year <= 2018 ~ "Enrique Peña Nieto (2012-2018)",
+           year > 2018 & year <= 2024 ~ "Andrés Manuel López Obrador (2018-2024)",
+           year > 2024 & year <= 2030 ~ "Claudia Sheinbaum Pardo (2024-2030)",
            TRUE ~ "Otro"
          ),
          sexenio = factor(sexenio, levels = orden_sexenios) # Ordenar como factor
@@ -60,13 +60,16 @@ sexenios_gdp <- gdp %>%
   arrange(sexenio)
 
 # Crecimiento económico
-ggplot(gdp, aes(date, growth)) +
-  geom_line(size = 1) +
+gdp %>% 
+  filter(date >= "2022-01-01") %>% 
+ggplot(aes(date, growth/100)) +
+  geom_line(size = 1, color = "#970639") +
   labs( title = "Crecimiento económico en México",
         subtitle = "Variación anual",
         y = "",
         x = "",
         caption = "Fuente: INEGI") +
+  scale_y_percent() +
   theme_ipsum_rc(grid="Y") %>% 
   gg_check()
 ggsave("plots/gdp_growth.png")
@@ -74,7 +77,7 @@ ggsave("plots/gdp_growth.png")
 
 # Crecimiento promedio por sexenio
 ggplot(sexenios_gdp, aes(mean_growth/100, fct_rev(sexenio))) +
-  geom_col() +
+  geom_col(fill = "#970639") +
   labs( title = "Crecimiento promedio por sexenio",
         subtitle = "Promedio del crecimiento anual en México",
         y = "",
@@ -95,7 +98,7 @@ gdppc <- left_join(gdp, pop, join_by(date), suffix = c("_gdp", "_pop")) %>%
 
 # Crecimiento económico per cápita
 ggplot(gdppc, aes(date, gdppc)) +
-  geom_line(size = 1) +
+  geom_line(size = 1, color = "#970639") +
   labs( title = "PIB per cápita en México",
         subtitle = "Moneda nacional",
         y = "",
@@ -105,3 +108,6 @@ ggplot(gdppc, aes(date, gdppc)) +
   theme_ipsum_rc(grid="Y") %>% 
   gg_check()
 ggsave("plots/gdppc.png")
+
+
+write.csv(gdppc, "data/growth/gdppc.csv", row.names = FALSE)
--- r/inflation/mc_inflation.R
+++ r/inflation/mc_inflation.R
@@ -5,7 +5,14 @@ rm(list = ls())
 library(inegiR)
 library(dplyr)
 library(tidyr)
+library(ggplot2)
+library(hrbrthemes)
+library(forcats)
 library(lubridate)
+library(sysfonts)
+
+font_add_google("Roboto Condensed")
+import_roboto_condensed()
 
 # Define your INEGI API key
 inegi.api = Sys.getenv("INEGI_API")
@@ -26,6 +33,26 @@ series.wide <- series %>%
     'Subyacente' = '910407',
     'No subyacente' = '910410') %>% 
   filter(date >= Sys.Date() - years(3))
+
+series.long <- pivot_longer(series.wide,
+                            cols = c("Inflación general", "Subyacente", "No subyacente"),
+                            names_to = "index",
+                            values_to = "values")
+
+ggplot(series.long, aes(date, values/100, color = index)) +
+  geom_line(size = 1) +
+  labs( title = "Crecimiento económico en México",
+        subtitle = "Variación anual",
+        y = "",
+        x = "",
+        caption = "Fuente: INEGI",
+        color = "") +
+  scale_y_percent() +
+  theme_ipsum_rc(grid="Y") +
+  scale_fill_flexoki_light() +
+  theme(legend.position = "bottom") %>% 
+  gg_check()
+ggsave("plots/gdp_growth.png")
 
 # Specify the output directory and file name
 write.csv(series.wide, "data/mc_inflation.csv", row.names = FALSE)
