88343cabdd60a0ccb94184bf1a4115a653e30dc9
Juan Alvaro <jadrk040507@gmail.com>
2025-02-18 10:47:20 -0600

    Fix iframe

--- r/gdp/global_gdp.R
+++ r/gdp/global_gdp.R
@@ -5,6 +5,9 @@ library(rsdmx)
 library(hrbrthemes)
 library(showtext)
 library(svglite)
+library(plotly)
+library(htmlwidgets)
+
 
 font_add_google("Roboto Condensed", "Roboto")
 showtext_auto()
@@ -20,7 +23,8 @@ names(growth)
 # Clean data
 growth_clean <- growth %>% 
   reframe(TIME_PERIOD, REF_AREA, obsValue) %>% 
-  arrange(desc(TIME_PERIOD), REF_AREA)
+  arrange(desc(TIME_PERIOD), REF_AREA) %>% 
+  mutate(fill_color = ifelse(REF_AREA == "MEX", "#931336", "grey"))
 
 # Note! Data will be updated on February 20, 2025
 
@@ -30,7 +34,7 @@ ggplot(growth_clean %>%
          filter(TIME_PERIOD == max(TIME_PERIOD),
                 REF_AREA %in% c("ARG", "BRA", "CAN", "CHL", "CHN", "COL", "ESP", "FRA", "GBR", "IND", "ITA", "JPN", "KOR", "MEX", "USA")), 
        # Continue ggplot
-       aes(TIME_PERIOD, obsValue/100, fill = REF_AREA)) + 
+       aes(obsValue/100, reorder(REF_AREA, obsValue), fill = fill_color)) + 
   geom_col(position = "dodge") + 
   # Labels
   labs(
@@ -41,12 +45,29 @@ ggplot(growth_clean %>%
     fill = "",
     caption = paste("Fuente: OECD. Última actualización", format(Sys.time(), '%d %b, %Y'))
   ) +
-  scale_y_percent(breaks = seq(min(growth_clean$obsValue), max(growth_clean$obsValue), by = 1/100)) +
-  scale_fill_manual(values = c("#66c2a5", "#fc8d62", "#8da0cb", "#e78ac3", "#a6d854",
-                               "#ffd92f", "#e5c494", "#b3b3b3", "#1b9e77", "#d95f02",
-                               "#7570b3", "#e7298a", "#66a61e", "#e6ab02", "#a6761d")) +
-  theme_ipsum_rc(grid = "Y")
+  scale_x_percent(breaks = seq(min(growth_clean$obsValue), max(growth_clean$obsValue), by = 1/100)) +
+  scale_fill_identity() +
+  theme_ipsum_rc(grid = "Y") +
+  theme(
+    legend.position = "none"
+  ) 
 
-ggsave("plots/gdp/oecd_gdp_growth.svg", width = 1200, height = 900, units = "px", dpi = 150,create.dir = TRUE)
+ggsave("plots/gdp/oecd_gdp_growth.svg", width = 1200, height = 900, units = "px", dpi = 150, create.dir = TRUE)
+
+
+# Necessary for interactive plots iframe
+ggplotly(tooltip = "none") %>%
+  # Recover caption
+  layout(annotations = list(
+      x = 1,  # Horizontal position (centered)
+      y = -0.2, # Vertical position (below the plot)
+      text = paste("Fuente: OECD. Última actualización", format(Sys.time(), '%d %b, %Y')),  # The caption text
+      showarrow = FALSE,  # No arrow pointing to the caption
+      xref = 'paper',  # Use paper coordinates (relative to the plot's paper size)
+      yref = 'paper'   # Use paper coordinates for the caption position
+    )) %>% 
+  config(displayModeBar = FALSE) %>% 
+  saveWidget(file = "plots/gdp/oecd_gdp_growth.html")
+
 
 
