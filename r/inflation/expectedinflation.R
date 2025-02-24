448fa2febdfd589581d1e29e6e8ea3f0b36de2c1
Juan Alvaro <jadrk040507@gmail.com>
2025-02-24 15:24:55 -0600

    Expected inflation

--- r/inflation/expectedinflation.R
+++ r/inflation/expectedinflation.R
@@ -46,9 +46,7 @@ ggplot(series.long, aes(value/100, date, color = index, group = date)) +
     color = "",
     caption = paste("Fuente: Banxico. Última actualización", format(Sys.time(), '%d %b, %Y'))
   ) +
-  scale_x_percent(breaks = seq(min(series.long$value, na.rm = TRUE), 
-                                 max(series.long$value, na.rm = TRUE), 
-                                 by = 0.005))  + 
+  scale_x_percent(labels = scales::percent_format(accuracy = 0.1)) +  # Two decimal places
   scale_color_manual(values=c("#009076", "#ffe59c", "#c71e1d")) + 
   theme_ipsum_rc(base_family = "Rubik") +
   theme(
