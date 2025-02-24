6633cd24c7029ea31ecb1cc59680d0377e6aa8ef
Juan Alvaro <jadrk040507@gmail.com>
2025-02-24 14:31:37 -0600

    Expected

--- r/inflation/expectedinflation.R
+++ r/inflation/expectedinflation.R
@@ -50,6 +50,8 @@ ggplot(series.long, aes(value/100, date, color = index, group = date)) +
   theme(
     legend.position = "bottom"
   )
+ggsave("plots/inflation/expectedinflation.svg",  width = 8, height = 6, create.dir = TRUE)
+
 
 # Specify the output directory and file name
 write.csv(series.df, "data/mc_expectedinflation.csv", row.names = FALSE)
